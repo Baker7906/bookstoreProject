4a816fbce1e71a1f5d12e759e766d7d027e40296
Ahmad Nassar <168649900+3mkmNassar@users.noreply.github.com>
2024-05-05 12:42:20 +0300

    alo

--- src/search.java
+++ src/search.java
@@ -1,4 +1,4 @@
-package com.bittercode.model;
+
 
 import java.io.Serializable;
 
@@ -6,40 +6,67 @@ public class search implements Serializable {
     private String barcode ; 
     private String name ; 
     private String author ;
+    private double price;
     private String genre;
- 
+    private int quantinty;
+    
     publlic search(String barcode , String name , String author, String genre){
         this.barcode = barcode;
         this.name = name ;
         this.author = author;
+        this.setPrice(price);
        this.genre = genre;
     } 
+    
     public search (){
         return super;
     }
+   
     public String getBarcode(){
         return barcode;
     }
+   
     public void setBarcode (String barcode){
         this.barcode = barcode;
     }
+   
     public String getName (){
         return name;
     }
+    
     public void setName(String name){
         this.name = name ;
     }
+    
     public String getAuthor(){
         return author;
     }
+   
     public void setAuthor(String author){
         this.author = author;
+    }
+    public double getPrice(){
+        return price;
+    }
+    /**
+     * @param price
+     */
+    public void setPrice(double price){
+        this.price = price;
     }
     public String getGenre (){
         return genre ;
     }
+   
     public void setGenre(String genre){
         this.genre = genre;
     }
-    
+   
+    public int getQuantity(){
+        return quantinty;
+    }
+   
+    public void setQuantity(int quantinty  ){
+        this.quantinty = quantinty;
+    }
 }
